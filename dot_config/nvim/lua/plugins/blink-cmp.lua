607ac714881c35e6962a38c9f8d05536346cc43a
Gaishi Hirota <mail@gaishi.dev>
2026-03-19 00:57:12 +0900

    update blink-cmp config for applying an item by pressing tab and enter

--- dot_config/nvim/lua/plugins/blink-cmp.lua
+++ dot_config/nvim/lua/plugins/blink-cmp.lua
@@ -4,9 +4,22 @@ return {
   event = "InsertEnter",
   version = "1.*",
   opts = {
+    keymap = {
+      preset = "default",
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
+    },
+    completion = {
+      list = {
+        selection = {
+          preselect = false,
+          auto_insert = false,
+        },
+      },
+    },
     sources = {
       default = { "lsp", "path", "buffer" },
     },
   },
 }
-
