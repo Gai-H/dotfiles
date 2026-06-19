6143d318e4b49d89d944e37cede73ba452af492b
Gaishi Hirota <mail@gaishi.dev>
2026-06-19 23:53:49 +0900

    refactor(kitty): remove unused mode choices
    Limit the Kitty mode selector to none and tmux now that nvim and zellij modes are not used.

--- dot_config/kitty/kittens/select-mode.py
+++ dot_config/kitty/kittens/select-mode.py
@@ -7,7 +7,7 @@ from kitty.boss import Boss
 
 VARIABLE_KEY = "mode"
 VALIABLE_VALUE_NONE = "none"
-VARIABLE_VALUES = (VALIABLE_VALUE_NONE, "nvim", "tmux", "zellij")
+VARIABLE_VALUES = (VALIABLE_VALUE_NONE, "tmux")
 CANCEL = "__cancel__"
 
 
