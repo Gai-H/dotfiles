4acc0bc89e11434852081c93028888f59853ddf2
Gaishi Hirota <mail@gaishi.dev>
2026-07-12 14:43:50 +0900

    refactor(nvim): format editor_config

--- dot_config/nvim/lua/editor_config.lua
+++ dot_config/nvim/lua/editor_config.lua
@@ -1,68 +1,19 @@
 local opt = vim.opt
-
--- 全角文字が半角文字と重ならないようにする
--- opt.ambiwidth = "double"
-
--- カーソルが存在する行にハイライトをあてる
+opt.clipboard:append("unnamedplus")
 opt.cursorline = true
-
--- インデント
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
+opt.exrc = true
 opt.expandtab = true
-opt.autoindent = true
-opt.smartindent = true
-
--- 行番号
+opt.ignorecase = true
+opt.matchtime = 1
 opt.number = true
 opt.relativenumber = true
-opt.wrap = true
-opt.termguicolors = true
-
--- エンコーディング
-opt.encoding = "utf-8"
-opt.fileencoding = "utf-8"
-
--- クリップボード
-opt.clipboard:append("unnamedplus")
-
--- コマンドライン
-opt.wildmenu = true
-opt.laststatus = 2
-opt.showcmd = true
-
--- 検索・置換
-opt.hlsearch = true
-opt.incsearch = true
-opt.ignorecase = true
-opt.smartcase = true
+opt.shiftwidth = 2
 opt.showmatch = true
-opt.matchtime = 1
+opt.smartcase = true
+opt.smartindent = true
+opt.softtabstop = 2
+opt.tabstop = 2
 
--- カラースキーム
-opt.termguicolors = true
-opt.background = "dark"
-
--- netrw の無効化
 local g = vim.g
 g.loaded_netrw = 1
 g.loaded_netrwPlugin = 1
-
--- yl, dl, cl, >l, <l などの追加
---- vim.keymap.set("o", "l", function()
----   local n = vim.v.count1
----   return (n > 1 and tostring(n) or "") .. "V"
---- end, { expr = true, silent = true, desc = "Use l as line text-object in operator-pending" })
-
--- escape from terminal mode
-vim.keymap.set('t', 'fj', "<C-`\\><C-n>", { noremap = true, silent = true })
-
--- buffer navigation
-vim.keymap.set("n", "<M-j>", "<cmd>bprevious<CR>", { noremap = true, silent = true, desc = "Previous buffer" })
-vim.keymap.set("n", "<M-k>", "<cmd>bnext<CR>", { noremap = true, silent = true, desc = "Next buffer" })
-vim.keymap.set("n", "<M-w>", "<cmd>bdelete<CR>", { noremap = true, silent = true, desc = "Delete buffer" })
-vim.keymap.set("n", "<M-t>", "<cmd>enew<CR>", { noremap = true, silent = true, desc = "New buffer" })
-
--- exrc
-vim.o.exrc = true
